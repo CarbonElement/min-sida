4f873e913b2c7088070c5c9c5ecf17df8f350b52
Khaled Ahmed <khaahm02@elevad.umea.se>
2025-03-11 13:44:47 +0100

    uppdatering igen

--- src/routes/Memory/+page.svelte
+++ src/routes/Memory/+page.svelte
@@ -4,20 +4,65 @@
     let flipCount = 0;
     let blueTurn = false;
     let cards = []
+    let cane = "https://media.istockphoto.com/id/1358130261/sv/vektor/realistic-christmas-candy-cane-with-red-stripes-cute-cartoon-lollypop-sweet-on-white.jpg?s=612x612&w=0&k=20&c=MvF5KapisQHEqCu1wEnOepIaN8AHKlcauyQbCA4-u9I="
+    let hat = "https://media.istockphoto.com/id/1066901056/vector/santa-claus-hat-flat-style-icon-vector-illustration.jpg?s=612x612&w=0&k=20&c=J2ijmJrS-Og6HscEaJN6vKJv-NqvZPv0QaLN1BnMblQ="
+    let reindeer = "https://media.istockphoto.com/id/1351976317/sv/vektor/christmas-deer-in-ugly-sweater.jpg?s=612x612&w=0&k=20&c=Q7msbklygwviLozCZ2obD0I4yD9D_2FDCEnMtPVLWYg="
+    let tree ="https://media.istockphoto.com/id/882074046/sv/vektor/cartoon-julgran-med-guldstj%C3%A4rna.jpg?s=612x612&w=0&k=20&c=jtOVp6wBm9gMRmGXjeoJ3rJwkKSVOPXa5wBPqvf5smQ="
+    let snowflake="https://media.istockphoto.com/id/1395535483/vector/snowflake-snow-cute-weather-realistic-icon-3d-cartoon.jpg?s=612x612&w=0&k=20&c=RICCR98vowV_OgnbKhJdc2s3mzSFJWQrjur0VRQ_t-c="
+    let mountain ="https://i.pinimg.com/736x/fd/09/f7/fd09f7c5093408bcdd7510347301920f.jpg"
+    let images = [cane, hat, reindeer, tree, snowflake, mountain]
+    let result = ""
     for (let i=0; i<12; i++) {
-        cards.push({image: "favicon.png", flipped: false, matched: false})
+        cards.push({image: images[i%6], flipped: false, matched: false})
     }
 
     function flip(card)  {
+        if(flipCount == 2) return;
+        if(card.flipped) return;
         card.flipped = !card.flipped
         cards = cards
+        flipCount++;
+        if(flipCount == 2) {
+            let flippedCards = cards.filter(card =>card.flipped && !card.matched)
+            if (flippedCards[0].image== flippedCards[1].image){
+                flippedCards[0].matched= true 
+                flippedCards[1].matched=true
+                if (blueTurn){
+                    bluePoints++
+                } else {
+                    redPoints++
+                }
+            }
+        
+            setTimeout(() => {
+                cards.forEach((card) => {
+                        card.flipped = card.matched;
+                });
+                flipCount = 0;
+                cards = cards;
+                blueTurn=!blueTurn
+                }, 1000);
+        }
+        if (bluePoints+redPoints==6){
+            if (bluePoints<redPoints){
+            result = "Red is the winner!"
+                
+            } else if (bluePoints>redPoints){
+                result = "Blue is the winner!"
+
+            } else {
+                result = "It's a tie"
+            }
+        }
+        
     }
 </script>
 <main>
     {#each cards as card}
     <div on:click={() => flip(card)} class="card" class:flipped={card.flipped}>
-    <img alt="front" class="front" src={card.image}>
-    <img alt="back" class="back" src="https://previews.123rf.com/images/captainvector/captainvector1703/captainvector170311727/74440143-lightning-with-cloud-icon.jpg">
+    
+    <img alt="back" class="front" src="https://media.istockphoto.com/id/493085772/vector/snowman.jpg?s=612x612&w=0&k=20&c=2VtC2Ag5GB3WQ5xo_jWKuqDzURXCl_e9H80P7KS7bM0=">
+    <img alt="front" class="back" src={card.image}>
     </div>
     {/each}
 </main>
@@ -30,6 +75,7 @@
 <aside class:blue = {blueTurn} class="turn">
     
 </aside>
+<p style="text-align: centre;">{result}</p>
 <style>
     main {
         margin: 30px;
